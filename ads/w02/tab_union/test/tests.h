d1d7ec913d21b0d8b7f4d8db90486a285ee3e5e0
Tony Emeltsev <aemeltsev@gmail.com>
2022-01-21 14:08:46 +0300

    update: update tests.h

---
+++ ads/w02/tab_union/test/tests.h
@@ -0,0 +1,15 @@
+#ifndef DISJOINTSET_TEST_H
+#define DISJOINTSET_TEST_H
+
+#include <iostream>
+#include <random>
+#include <vector>
+#include <gtest/gtest.h>
+#include <functional>
+#include "disjointset.h"
+
+namespace dsu {
+
+} //dsu
+
+#endif //DISJOINTSET_TEST_H
