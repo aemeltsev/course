22f9e503112db88a1e907f6c43a816052a97166e
Tony Emeltsev <aemeltsev@gmail.com>
2022-01-22 20:35:37 +0300

    update: update tests.h

---
+++ ads/w03/hash_note/test/tests.h
@@ -0,0 +1,16 @@
+#ifndef NHASH_TEST_H
+#define NHASH_TEST_H
+
+#include <iostream>
+#include <random>
+#include <vector>
+#include <gtest/gtest.h>
+#include <functional>
+#include "nhash.h"
+
+namespace hash
+{
+
+}
+
+#endif //NHASH_TEST_H
